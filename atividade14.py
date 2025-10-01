0b778eb1a2a33f256d652bb9df91194280a0cfce
eli <eliadadearaujolinhares@gmail.com>
2025-10-01 18:38:33 -0300

    Add files via upload

---
+++ atividade14.py
@@ -0,0 +1,5 @@
+# Crie uma tupla com os dias da semana e exiba o terceiro e o sexto dia.
+
+dias_da_semana = ("Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo")
+print("Terceiro dia:", dias_da_semana[2])
+print("Sexto dia:", dias_da_semana[5])
